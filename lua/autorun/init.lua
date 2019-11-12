4bc82909019d487dad76527b6032458de288b844
Earu <earu.derg@gmail.com>
2019-11-12 22:05:50 +0100

    Exclude bad formats

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,13 +1,28 @@
 AddCSLuaFile()
 
 if SERVER then
+	local blacklist = {
+		"ztmp",
+		"bz2"
+	}
+
+	local function IsBadFile(f)
+		for _, format in pairs(blacklist) do
+			if f:EndsWith(format) then return true end
+		end
+
+		return false
+	end
+
 	local function AddResourceDir(dir)
 		if not file.Exists(dir, "GAME") then return end
 
 		local dirs, files = file.Find(dir .. "/*","GAME")
-        for _,f in pairs(files) do
-            local path = dir .. "/" .. f
-            resource.AddFile(path)
+		for _,f in pairs(files) do
+			if not IsBadFile(f) then
+				local path = dir .. "/" .. f
+				resource.AddFile(path)
+			end
 		end
 
 		for _, d in pairs(dirs) do
